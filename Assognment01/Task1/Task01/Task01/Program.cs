25c89c2e13faa08bc3f1faff05d4cc7d8f618c0c
levan09 <leolapachi09@gmail.com>
2023-06-04 23:12:24 +0400

    Commit 1

---
+++ Assognment01/Task1/Task01/Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("ra temperaturaa dges?");
+string temperature = Console.ReadLine();
+
+int TempInt = int.Parse(temperature);
+
+if(TempInt < 0)
+{
+    Console.WriteLine("yinavs");
+    return;
+}else if(TempInt >= 0 &&  TempInt <= 30)
+{
+    Console.WriteLine("kargi amindia");
+    return;
+}
+else
+{
+    Console.WriteLine("cxela");
+}
